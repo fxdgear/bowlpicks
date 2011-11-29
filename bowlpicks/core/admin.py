21bd7735f46e9cff2d07b24f0ed72790e4bfdb75
Nick Lang <nick.lang@gmail.com>
2011-11-29 14:51:06 -0700

    adding game admin details

--- bowlpicks/core/admin.py
+++ bowlpicks/core/admin.py
@@ -3,9 +3,20 @@ from bowlpicks.core.models import Season, Conference, School, Team, Game, Pick
 from bowlpicks.core import listeners
 
 
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('season__year_end', )
+    list_display = (
+        'name',
+        'date',
+        'season',
+        'home_team',
+        'home_score',
+        'away_team',
+        'away_score',)
+
 admin.site.register(Season)
 admin.site.register(Conference)
 admin.site.register(School)
 admin.site.register(Team)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Pick)
