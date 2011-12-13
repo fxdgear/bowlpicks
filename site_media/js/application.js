abd0ea520daacc581b760372f4fd08ec9ea36a03
Nick Lang <nick.lang@gmail.com>
2011-12-13 01:05:51 -0700

    undoing the twittebootstrap blocking

--- site_media/js/application.js
+++ site_media/js/application.js
@@ -69,14 +69,6 @@ $(document).ready(function(){
     }
   });
 
-
-  // Disable certain links in docs
-  // =============================
-
-  $('ul.tabs a, ul.pills a, .pagination a, .well .btn, .actions .btn, .alert-message .btn, a.close').click(function(e) {
-    e.preventDefault();
-  });
-
   // Copy code blocks in docs
   $(".copy-code").focus(function() {
     var el = this;
