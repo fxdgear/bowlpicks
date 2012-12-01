995177b1be51593f25d6935762498fa9a89c4212
Nick Lang <nick.lang@gmail.com>
2012-11-30 21:27:40 -0700

    adding password reset functionlity

--- bowlpicks/urls.py
+++ bowlpicks/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,6 +13,22 @@ from bowlpicks.profiles.views import profile_detail, delete_player
 urlpatterns = patterns('',
     url(r'^$', HomePage.as_view(), {}, 'homepage'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/password/reset/$',
+        password_reset,
+        {'template_name': 'registration/forgot_password.html'},
+        name='password_reset'),
+    url(r'^accounts/password/reset/done/$',
+        password_reset_done,
+        {'template_name': 'registration/password_reset_done.html'}),
+    url(r'^accounts/password/reset/done/$',
+        password_reset_done,
+        {'template_name': 'registration/password_reset_done.html'}),
+    url(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        password_reset_confirm,
+        {'template_name': 'registration/password_reset_confirm.html'}),
+    url(r'^reset/done/$',
+        password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'}),
     url(r'^accounts/', include('registration.backends.default.urls')),
     (r'^contact/thankyou/$', 'bowlpicks.contact.views.thankyou'),
     (r'^contact/$', 'bowlpicks.contact.views.contactview', {}, 'contact'),
